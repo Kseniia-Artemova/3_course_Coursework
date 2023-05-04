dfc28201e555d2529138ee47c3420540251f9d61
Ksu <kseniyaartemova@yandex.ru>
2023-05-04 18:14:51 +0200

    added methods in class payment and some tests for it

--- scr/payment.py
+++ scr/payment.py
@@ -25,31 +25,44 @@ class Payment:
         return self.__state
 
     def set_date(self, date):
-        date_for_format = str(date).split("T")[0].split("-")[::-1]
-
-        if len(date_for_format) == 3:
-            if all([
-                len(date_for_format[0]) == 2,
-                date_for_format[0].isdigit() and int(date_for_format[0]) in range(1, 32),
-                len(date_for_format[1]) == 2,
-                date_for_format[1].isdigit() and int(date_for_format[1]) in range(1, 13),
-                len(date_for_format[2]) == 4,
-                date_for_format[2].isdigit()
-            ]):
-
-                self.__date = ".".join(date_for_format)
-            else:
-                self.__date = "Invalid date format!"
-
+        date = str(date).split("T")[0].split("-")[::-1]
+        if self.__check_date(date):
+            self.__date = ".".join(date)
         else:
             self.__date = "Invalid date format!"
+
+    @staticmethod
+    def __check_date(date):
+        days_in_months = {
+            "01": 31, "02": 29, "03": 31, "04": 30, "05": 31, "06": 30,
+            "07": 31, "08": 31, "09": 30, "10": 31, "11": 30, "12": 31
+        }
+
+        if len(date) != 3:
+            return False
+        elif not date[0].isdigit() or len(date[0]) != 2:
+            return False
+        elif date[1] not in days_in_months:
+            return False
+        elif int(date[0]) not in range(1, days_in_months.get(date[1]) + 1):
+            return False
+        elif len(date[2]) != 4 or not date[2].isdigit():
+            return False
+
+        return True
 
     def get_date(self):
         return self.__date
 
     def set_operation_amount(self, operation_amount):
+        pass
+        amount = operation_amount.get('amount')
+        currency = operation_amount.get('currency')
+        currency_name = currency.get('name') if currency else None
 
-        self.__operation_amount = operation_amount
+        operation_amount_new = OperationAmount(amount, currency_name)
+
+        self.__operation_amount = operation_amount_new
 
     def get_operation_amount(self):
         return self.__operation_amount
@@ -75,12 +88,30 @@ class Payment:
 
 class OperationAmount:
 
-    def __init__(self, dict_):
+    def __init__(self, amount, currency):
         self.__amount = None
-        self.__currency_name = None
+        self.set_amount(amount)
+        self.__currency_name = currency
 
     def set_amount(self, amount):
-        self.__amount = amount
+        if amount and self.__check_amount(amount):
+            self.__amount = amount
+        else:
+            self.__amount = "Incorrect amount!"
+
+    @staticmethod
+    def __check_amount(amount):
+        amount = str(amount).split(".")
+        if len(amount) != 2:
+            return False
+        elif len(amount[1]) != 2:
+            return False
+        elif not amount[0].isdigit() or not amount[1].isdigit():
+            return False
+        elif int(amount[0]) < 0 or int(amount[1]) < 0:
+            return False
+
+        return True
 
     def get_amount(self):
         return self.__amount
