6f6804e44968c8c7e64f10e4be010cda97b8a242
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-05-05 02:51:24 +0200

    finished class Payment, added new tests, created main.py, added new functions in utils.py (#3)

--- scr/utils.py
+++ scr/utils.py
@@ -1,11 +1,25 @@
 import json
+import scr.payment as pay_
 
-
-def open_json(path):
+def open_payments(path, revert=False):
     with open(path, "rt", encoding="utf-8") as json_file:
         payments = json.load(json_file)
-    return payments
+
+    return payments[::-1] if revert else payments
 
 
-for i in open_json("../sources/operations.json"):
-    print(i)
+def create_payment(pay_information: dict):
+    payment = pay_.Payment()
+    payment.set_id(pay_information.get("id"))
+    payment.set_state(pay_information.get("state"))
+    payment.set_date(pay_information.get("date"))
+    payment.set_operation_amount(pay_information.get("operationAmount"))
+    payment.set_description(pay_information.get("description"))
+    payment.set_pay_from(pay_information.get("from"))
+    payment.set_pay_to(pay_information.get("to"))
+
+    return payment
+
+
+def show_payment(pay):
+    pass
--- tests/test_paiment.py
+++ tests/test_paiment.py
@@ -33,25 +33,76 @@ def test_set_operation_amount_correct(pay):
     pay.set_operation_amount({'amount': '56516.63', 'currency': {'name': 'USD', 'code': 'USD'}})
     operation_amount = pay.get_operation_amount()
     assert operation_amount.get_amount() == "56516.63"
+    assert operation_amount.get_currency_name() == "USD"
 
 
 def test_set_operation_amount_incorrect(pay):
     pay.set_operation_amount({'amount': '56516.3', 'currency': {'name': 'USD', 'code': 'USD'}})
     operation_amount = pay.get_operation_amount()
     assert operation_amount.get_amount() == "Incorrect amount!"
+    assert operation_amount.get_currency_name() == "USD"
 
-    pay.set_operation_amount({'amount': '56516.a3', 'currency': {'name': 'USD', 'code': 'USD'}})
+    pay.set_operation_amount({'amount': '56516.a3', 'currency': {'name': 'руб.', 'code': 'RUB'}})
     operation_amount = pay.get_operation_amount()
     assert operation_amount.get_amount() == "Incorrect amount!"
+    assert operation_amount.get_currency_name() == "руб."
 
-    pay.set_operation_amount({'amount': '-56516.03', 'currency': {'name': 'USD', 'code': 'USD'}})
+    pay.set_operation_amount({'amount': '-56516.03', 'currency': {'name': '', 'code': 'USD'}})
     operation_amount = pay.get_operation_amount()
     assert operation_amount.get_amount() == "Incorrect amount!"
+    assert operation_amount.get_currency_name() is None
 
-    pay.set_operation_amount({'amount': '565163', 'currency': {'name': 'USD', 'code': 'USD'}})
+    pay.set_operation_amount({'amount': '565163', 'currency': {'name': 0, 'code': 'USD'}})
     operation_amount = pay.get_operation_amount()
     assert operation_amount.get_amount() == "Incorrect amount!"
+    assert operation_amount.get_currency_name() is None
 
 
+def test_set_description(pay):
+    pay.set_description('Перевод с карты на карту')
+    assert pay.get_description() == 'Перевод с карты на карту'
 
 
+def test_set_description_none(pay):
+    pay.set_description("")
+    assert pay.get_description() is None
+
+
+def test_set_description_not_str(pay):
+    pay.set_description(True)
+    assert pay.get_description() is None
+
+
+def test_set_pay_from(pay):
+    pay.set_pay_from('Счет 33355011456314142963')
+    assert pay.get_pay_from() == 'Счет 33355011456314142963'
+
+
+def test_set_pay_from_empty(pay):
+    pay.set_pay_from('')
+    assert pay.get_pay_from() == "Incorrect account or card number!"
+
+
+def test_set_pay_from_incorrect(pay):
+    pay.set_pay_from('Maestro 8609654751155')
+    assert pay.get_pay_from() == "Incorrect account or card number!"
+
+
+def test_set_pay_to(pay):
+    pay.set_pay_to('Maestro 8602249654751155')
+    assert pay.get_pay_to() == 'Maestro 8602249654751155'
+
+
+def test_set_pay_to_empty(pay):
+    pay.set_pay_to('')
+    assert pay.get_pay_to() == "Incorrect account or card number!"
+
+
+def test_set_pay_to_too_short_number(pay):
+    pay.set_pay_to('Счет 4573591729755908868')
+    assert pay.get_pay_to() == "Incorrect account or card number!"
+
+
+def test_set_pay_to_incorrect_number(pay):
+    pay.set_pay_to('МИР 521127741822846A9')
+    assert pay.get_pay_to() == "Incorrect account or card number!"
