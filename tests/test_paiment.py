85cb63a143f4e44b744dc895e1db642026c38162
Ksu <kseniyaartemova@yandex.ru>
2023-05-08 15:44:04 +0200

    added tests for Payment

--- tests/test_paiment.py
+++ tests/test_paiment.py
@@ -1,109 +1,286 @@
 import pytest
-import scr.payment as pay_
+from scr.payment import Payment
+from datetime import datetime
 
 
 @pytest.fixture
 def pay():
-    return pay_.Payment()
+    return Payment()
 
 
-def test_set_date_correct(pay):
-    pay.set_date('2019-08-26T10:50:58.294041')
-    assert pay.get_date() == "26.08.2019"
+def test___str__(pay):
+    pay.id_pay = 542678139
+    assert str(pay) == f"Payment 542678139, more detailed information is closed."
 
 
-def test_set_date_incorrect(pay):
-    pay.set_date('2019-08-26C10:50:58.294041')
-    assert pay.get_date() == "Invalid date format!"
-    pay.set_date('2019-08-2T10:50:58.294041')
-    assert pay.get_date() == "Invalid date format!"
-    pay.set_date('201A-08-20T10:50:58.294041')
-    assert pay.get_date() == "Invalid date format!"
-    pay.set_date('20190-08-20T10:50:58.294041')
-    assert pay.get_date() == "Invalid date format!"
-    pay.set_date('2019008-20T10:50:58.294041')
-    assert pay.get_date() == "Invalid date format!"
-    pay.set_date('')
-    assert pay.get_date() == "Invalid date format!"
-    pay.set_date('2019-04-31T10:50:58.294041')
-    assert pay.get_date() == "Invalid date format!"
+def test___repr__(pay):
+    assert repr(pay) == 'Payment(id_pay=None,state_pay="None",date_pay="None",operation_amount_pay=None,' \
+                  'description_pay="None",from_pay="None",to_pay="None")'
 
 
-def test_set_operation_amount_correct(pay):
-    pay.set_operation_amount({'amount': '56516.63', 'currency': {'name': 'USD', 'code': 'USD'}})
-    assert pay.get_operation_amount() == ("56516.63", "USD")
+def test_id_pay_correct(pay):
+    pay.id_pay = 441945886
+    assert pay.id_pay == 441945886
 
 
-def test_set_operation_amount_incorrect_amount_1(pay):
-    pay.set_operation_amount({'amount': '56516.3', 'currency': {'name': 'USD', 'code': 'USD'}})
-    assert pay.get_operation_amount() == "Incorrect amount!"
+def test_id_pay_incorrect(pay):
+    pay.id_pay = "441945886"
+    assert pay.id_pay is None
+
+    pay.id_pay = True
+    assert pay.id_pay is None
+
+    pay.id_pay = 4419458.86
+    assert pay.id_pay is None
 
 
-def test_set_operation_amount_incorrect_amount_2(pay):
-    pay.set_operation_amount({'amount': '-56516.30', 'currency': {'name': 'USD', 'code': 'USD'}})
-    assert pay.get_operation_amount() == "Incorrect amount!"
+def test_state_pay_executed(pay):
+    pay.state_pay = "EXECUTED"
+    assert pay.state_pay == "EXECUTED"
+
+    pay.state_pay = "executed"
+    assert pay.state_pay == "EXECUTED"
 
 
-def test_set_operation_amount_incorrect_amount_3(pay):
-    pay.set_operation_amount({'amount': '', 'currency': {'name': 'USD', 'code': 'USD'}})
-    assert pay.get_operation_amount() == "Incorrect amount or currency!"
+def test_state_pay_canceled(pay):
+    pay.state_pay = "CANCELED"
+    assert pay.state_pay == "CANCELED"
+
+    pay.state_pay = "canceled"
+    assert pay.state_pay == "CANCELED"
 
 
-def test_set_description(pay):
-    pay.set_description('Перевод с карты на карту')
-    assert pay.get_description() == 'Перевод с карты на карту'
+def test_state_pay_incorrect(pay):
+    pay.state_pay = "CANCEL"
+    assert pay.state_pay is None
+
+    pay.state_pay = "успешная"
+    assert pay.state_pay is None
+
+    pay.state_pay = 1
+    assert pay.state_pay is None
+
+    pay.state_pay = True
+    assert pay.state_pay is None
 
 
-def test_set_operation_amount_incorrect_amount_4(pay):
-    pay.set_operation_amount({'amount': '', 'currency': {}})
-    assert pay.get_operation_amount() == "Incorrect amount or currency!"
+def test_date_pay_correct(pay):
+    pay.date_pay = "2018-10-14T08:21:33.419441"
+    assert pay.date_pay == datetime(2018, 10, 14, 8, 21, 33, 419441)
 
 
-def test_set_operation_amount_incorrect_name(pay):
-    pay.set_operation_amount({'amount': '56516.30', 'currency': {'name': '', 'code': 'USD'}})
-    assert pay.get_operation_amount() == "Incorrect amount or currency!"
+def test_date_pay_incorrect(pay):
+    pay.date_pay = "2018-13-14T08:21:33.419441"
+    assert pay.date_pay is None
+
+    pay.date_pay = "2018-10-14T28:21:33.419441"
+    assert pay.date_pay is None
+
+    pay.date_pay = "2018-11-31T08:21:33.419441"
+    assert pay.date_pay is None
+
+    pay.date_pay = 20181131
+    assert pay.date_pay is None
+
+    pay.date_pay = "2012-02-30T08:21:33.419441"
+    assert pay.date_pay is None
+
+    pay.date_pay = "2013-02-29T08:21:33.419441"
+    assert pay.date_pay is None
+
+    pay.date_pay = "2013-02-29"
+    assert pay.date_pay is None
+
+    pay.date_pay = True
+    assert pay.date_pay is None
 
 
-def test_set_description_none(pay):
-    pay.set_description("")
-    assert pay.get_description() is None
+def test_operation_amount_pay_correct(pay):
+    pay.operation_amount_pay = {
+      "amount": "77751.04",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    }
+    assert pay.operation_amount_pay == ("77751.04", "руб.")
+
+    pay.operation_amount_pay = {
+      "amount": "77751",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    }
+    assert pay.operation_amount_pay == ("77751.00", "руб.")
+
+    pay.operation_amount_pay = {
+      "amount": 77751,
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    }
+    assert pay.operation_amount_pay == ("77751.00", "руб.")
 
 
-def test_set_description_not_str(pay):
-    pay.set_description(True)
-    assert pay.get_description() is None
+def test_operation_amount_pay_incorrect(pay):
+    pay.operation_amount_pay = {}
+    assert pay.operation_amount_pay is None
+
+    pay.operation_amount_pay = {
+      "amount": "77751.04",
+      "currency": {
+        "name": 1,
+        "code": "RUB"
+      }
+    }
+    assert pay.operation_amount_pay is None
+
+    pay.operation_amount_pay = {
+        "amount": "77751.04",
+        "currency": {
+            "name": "",
+            "code": "RUB"
+        }
+    }
+    assert pay.operation_amount_pay is None
+
+    pay.operation_amount_pay = {
+        "amount": "77751.04",
+        "currency": {}
+    }
+    assert pay.operation_amount_pay is None
+
+    pay.operation_amount_pay = {
+        "amount": True,
+        "currency": {
+            "name": "руб.",
+            "code": "RUB"
+        }
+    }
+    assert pay.operation_amount_pay is None
+
+    pay.operation_amount_pay = {
+        "amount": False,
+        "currency": {
+            "name": "руб.",
+            "code": "RUB"
+        }
+    }
+    assert pay.operation_amount_pay is None
+
+    pay.operation_amount_pay = {
+        "amount": "тридцать",
+        "currency": {
+            "name": "руб.",
+            "code": "RUB"
+        }
+    }
+    assert pay.operation_amount_pay is None
 
 
-def test_set_pay_from(pay):
-    pay.set_pay_from('Счет 33355011456314142963')
-    assert pay.get_pay_from() == ("Счет", "33355011456314142963")
+def test_description_pay_correct(pay):
+    pay.description_pay = "Перевод с карты на счет"
+    assert pay.description_pay == "Перевод с карты на счет"
 
 
-def test_set_pay_from_empty(pay):
-    pay.set_pay_from('')
-    assert pay.get_pay_from() is None
+def test_description_pay_incorrect(pay):
+    pay.description_pay = True
+    assert pay.description_pay is None
+
+    pay.description_pay = 4
+    assert pay.description_pay is None
 
 
-def test_set_pay_from_incorrect(pay):
-    pay.set_pay_from('Maestro 8609654751155')
-    assert pay.get_pay_from() == "Incorrect account or card number!"
+def test_from_pay_correct(pay):
+    pay.from_pay = "Maestro 4598300720424501"
+    assert pay.from_pay == ("Maestro", "4598300720424501")
+
+    pay.from_pay = "Visa Gold 9447344650495960"
+    assert pay.from_pay == ("Visa Gold", "9447344650495960")
+
+    pay.from_pay = "Счет 97584898735659638967"
+    assert pay.from_pay == ("Счет", "97584898735659638967")
 
 
-def test_set_pay_to(pay):
-    pay.set_pay_to('Maestro 8602249654751155')
-    assert pay.get_pay_to() == ("Maestro", "8602249654751155")
+def test_from_pay_incorrect(pay):
+    pay.from_pay = "Maestro4598300720424501"
+    assert pay.from_pay is None
+
+    pay.from_pay = "Maestro-4598300720424501"
+    assert pay.from_pay is None
+
+    pay.from_pay = "Visa-Gold-944734465095960"
+    assert pay.from_pay is None
+
+    pay.from_pay = 4598300720424501
+    assert pay.from_pay is None
+
+    pay.from_pay = True
+    assert pay.from_pay is None
+
+    pay.from_pay = "Visa Gold 944734465095960"
+    assert pay.from_pay is None
+
+    pay.from_pay = "Visa Platinum 2256483756A542539"
+    assert pay.from_pay is None
+
+    pay.from_pay = "Счет E97584898735659638967"
+    assert pay.from_pay is None
+
+    pay.from_pay = "Visa Gold 9758489873566301020067"
+    assert pay.from_pay is None
+
+    pay.from_pay = "Счет 9758489873566301020067"
+    assert pay.from_pay is None
+
+    pay.from_pay = "Счет 9758489873566367"
+    assert pay.from_pay is None
 
 
-def test_set_pay_to_empty(pay):
-    pay.set_pay_to('')
-    assert pay.get_pay_to() is None
+def test_to_pay_correct(pay):
+    pay.to_pay = "Maestro 4598300720424501"
+    assert pay.to_pay == ("Maestro", "4598300720424501")
+
+    pay.to_pay = "Visa Gold 9447344650495960"
+    assert pay.to_pay == ("Visa Gold", "9447344650495960")
+
+    pay.to_pay = "Счет 97584898735659638967"
+    assert pay.to_pay == ("Счет", "97584898735659638967")
 
 
-def test_set_pay_to_too_short_number(pay):
-    pay.set_pay_to('Счет 4573591729755908868')
-    assert pay.get_pay_to() == "Incorrect account or card number!"
+def test_to_pay_incorrect(pay):
+    pay.to_pay = "Maestro4598300720424501"
+    assert pay.to_pay is None
 
+    pay.to_pay = "Maestro-4598300720424501"
+    assert pay.to_pay is None
 
-def test_set_pay_to_incorrect_number(pay):
-    pay.set_pay_to('МИР 521127741822846A9')
-    assert pay.get_pay_to() == "Incorrect account or card number!"
+    pay.to_pay = "Visa-Gold-944734465095960"
+    assert pay.to_pay is None
+
+    pay.to_pay = 4598300720424501
+    assert pay.to_pay is None
+
+    pay.to_pay = True
+    assert pay.to_pay is None
+
+    pay.to_pay = "Visa Gold 944734465095960"
+    assert pay.to_pay is None
+
+    pay.to_pay = "Visa Platinum 2256483756A542539"
+    assert pay.to_pay is None
+
+    pay.to_pay = "Счет E97584898735659638967"
+    assert pay.to_pay is None
+
+    pay.to_pay = "Visa Gold 9758489873566301020067"
+    assert pay.to_pay is None
+
+    pay.to_pay = "Счет 9758489873566301020067"
+    assert pay.to_pay is None
+
+    pay.to_pay = "Счет 9758489873566367"
+    assert pay.to_pay is None
+
