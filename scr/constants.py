b60510225961ba56810a7aac58cafb58876fb9a1
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-05-11 14:05:45 +0200

    added descriptions for constants (#14)

--- scr/constants.py
+++ scr/constants.py
@@ -1,5 +1,14 @@
-PATH = "sources/operations.json"  # путь к файлу с операциями от корневой папки проекта
+# путь к файлу с операциями от корневой папки проекта
+PATH = "sources/operations.json"
+
+# количество платежей для отображения на экране
 COUNT_TRANSFERS = 5
+
+# обязательные поля, которые должны быть у платежа
 OBLIGATION_PARAMETERS_PAY = {"id", "date", "state", "operationAmount", "description", "to"}
-FORMAT_DATE = "%Y-%m-%dT%H:%M:%S.%f"    # принятый формат даты
+
+# принятый формат даты
+FORMAT_DATE = "%Y-%m-%dT%H:%M:%S.%f"
+
+# корректное количество символов номера для карты и счета
 AMOUNT_DIGITS = {"card": 16, "account": 20}
